bee875288cf98d9a0d7d8e8c7d0dba7e7de98d5b
Trevor Bedford <trevor@bedford.io>
2020-10-28 15:26:20 -0700

    Update with clade-level analysis of Pangolin lineage B.1.26

---
+++ scripts/identify-matching-haplotypes.py
@@ -0,0 +1,39 @@
+"""
+Identify matching haplotypes
+"""
+import argparse
+from Bio import AlignIO, SeqIO
+from Bio.Align import MultipleSeqAlignment
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Identify matching haplotypes",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('--alignment', type=str, metavar="FASTA", required=True, help="input FASTA alignment")
+    args = parser.parse_args()
+
+    aln = AlignIO.read(args.alignment, "fasta")
+    ref = [a for a in aln if a.id=='Wuhan/Hu-1/2019'][0]
+
+    ref_expected = ['C', 'C']
+    positions = [20402, 1059]
+    wt_expected = ['T', 'T']
+
+    for i in range(0, len(positions)):
+        assert(ref[positions[i]-1]==ref_expected[i])
+
+    newwave_records = []
+    for seq in aln:
+        include = True
+        for i in range(0, len(positions)):
+            if seq[positions[i]-1]!=wt_expected[i]:
+                include = False
+        if include:
+            newwave_records.append(seq)
+            print(seq.name)
+
+    # for record in newwave_records:
+    #     print(record)
